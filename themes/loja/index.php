24154529bdba06cfef64f6d70fd2c2e4c5d13b83
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2024-06-25 10:33:02 -0300

    Estruturação da Página Inicial da Loja Virtual

--- themes/loja/index.php
+++ themes/loja/index.php
@@ -1,1 +1,16 @@
+<?php include_once 'modal.php';?>
 <?php include_once 'header.php';?>
+
+<main class="container">
+    <section class="container_main">
+        <div class="container_controller">
+            <?php require 'pages-container/container-top.php';?>
+            <?php require 'pages-container/container-bestsellers.php';?>
+            <?php require 'pages-container/container-banner-small.php';?>
+            <?php require 'pages-container/container-foryou.php';?>
+            <?php require 'pages-container/container-thebest.php';?>
+        </div>
+    </section>
+</main>
+
+<?php include_once 'footer.php';?>
